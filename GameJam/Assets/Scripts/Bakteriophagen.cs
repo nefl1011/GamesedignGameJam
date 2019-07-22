69f706e3e6d1aab01399d6975877e10264594b04
Floh <florian@neuweiler-online.de>
2019-07-22 18:12:19 +0200

    Added scripts

---
+++ GameJam/Assets/Scripts/Bakteriophagen.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bakteriophagen : MonoBehaviour, Virus
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Jump()
+    {
+
+    }
+
+    public void Move()
+    {
+
+    }
+
+    public void Infect()
+    {
+
+    }
+
+    public void Die()
+    {
+
+    }
+
+    public void Respawn()
+    {
+
+    }
+
+    public void Spawn()
+    {
+
+    }
+}
---
+++ GameJam/Assets/Scripts/Virus.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface Virus
+{
+    void Jump();
+
+    void Move();
+
+    void Infect();
+
+    void Die();
+
+    void Respawn();
+
+    void Spawn();
+}
