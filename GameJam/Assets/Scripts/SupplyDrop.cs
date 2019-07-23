6c11dec6c49215c562494916e1d004c1608f5c4b
Marko <marko-mail@gmx.de>
2019-07-23 15:04:49 +0200

    Auto stash before merge of "master" and "origin/master"

---
+++ GameJam/Assets/Scripts/SupplyDrop.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class SupplyDrop : MonoBehaviour
+{
+    [Header("Settings")]
+    public int itemNo = 0;
+    public int amount = 30;
+    public int dropNo = 0;
+
+    public void AddAmmo()
+    {
+        ARPlayer.instance.ReceiveAmmo(itemNo, amount);
+        GameController.instance.Caller_DestroySupply(dropNo);
+        enabled = false;
+    }
+}
